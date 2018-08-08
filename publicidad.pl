b7ba89ee96ffedce28135e0cca8a97326e72cd62
Cam Palomino <camilarpalomino@gmail.com>
2018-08-08 20:16:19 -0300

    Punto 2
    Issues #2

--- publicidad.pl
+++ publicidad.pl
@@ -12,3 +12,16 @@ tipoDePublicidad(netflix, tranqui(4, habitación)).
 
 marca(Marca):- tipoDePublicidad(Marca, _).
 publicidad(Publicidad):- tipoDePublicidad(_, Publicidad).
+
+
+%Punto 2
+%tipoDePublicidad(bmw,_)
+%tipoDePublicidad(Marca, sexy(70)).
+
+:- begin_test(tipoDePublicidad).
+  test(bmwNoSePublicita):-
+   not(tipoDePublicidad(bmw,_)).
+  test(marcaSexy70Es212):-
+    tipoDePublicidad(Marca, sexy(70)).
+    Marca is 212.
+:-end_test(tipoDePublicidad).
